1c0d43f2e10a63521e257b479c29112a8c5f2a68
teclan <tbj621@163.com>
2019-01-02 16:52:30 +0800

    SpringBoot结合mybatis

--- src/main/java/teclan/springboot/Main.java
+++ src/main/java/teclan/springboot/Main.java
@@ -1,14 +1,16 @@
 package teclan.springboot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class) 
+@SpringBootApplication(scanBasePackages="teclan.springboot",exclude = DataSourceAutoConfiguration.class)
 //@EnableScheduling
 //@PropertySource(value = { "application.yml"})// 指定读取配置文件的路径
+@MapperScan("teclan.springboot.dao")
 public class Main {
 	public static void main(String[] args) {
 		 SpringApplication.run(Main.class, args);
--- src/main/java/teclan/springboot/controller/UserController.java
+++ src/main/java/teclan/springboot/controller/UserController.java
@@ -5,20 +5,29 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import teclan.springboot.model.User;
+import teclan.springboot.dao.Users1Mapper;
+import teclan.springboot.entity.Users1;
 
 @RestController  
 public class UserController {
 
 	@Resource
 	private JdbcTemplate jdbcTemplate;
+	@Autowired
+	private Users1Mapper users1Mapper;
 	
 	@RequestMapping("/user/query")
 	public List<Map<String,Object>> query() {
 		return jdbcTemplate.queryForList("select * from users limit 1,3");
 	}
+
+	@RequestMapping("/user/get")
+	public Users1 get(Long id) {
+		return users1Mapper.selectByPrimaryKey(1L);
+	}
+
 }
