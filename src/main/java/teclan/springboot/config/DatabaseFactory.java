89bd6df8c2377a7eca6c72afafb2636306b5719a
teclan <tbj621@163.com>
2019-01-04 10:08:36 +0800

    修复配置文件制定sqlmapper xml不生效的问题

--- src/main/java/teclan/springboot/config/DatabaseFactory.java
+++ src/main/java/teclan/springboot/config/DatabaseFactory.java
@@ -16,48 +16,53 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseFactory {
-	
-	private @Value("${datasource.driver-class-name}") String driver;
-	private @Value("${datasource.url}") String url;
-	private @Value("${datasource.username}") String username;
-	private @Value("${datasource.password}") String password;
+
+    private @Value("${datasource.driver-class-name}")
+    String driver;
+    private @Value("${datasource.url}")
+    String url;
+    private @Value("${datasource.username}")
+    String username;
+    private @Value("${datasource.password}")
+    String password;
+
+    private @Value("${mybatis.mapper-locations}")
+    String mapperLocations;
+
+    @Bean(name = "dataSource")
+    public DataSource getDataSource() {
+
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName(driver);
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+
+        return new HikariDataSource(config);
+    }
+
+    @Bean(name = "jdbcTemplate")
+    public JdbcTemplate getJdbcTemplate() {
+
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName(driver);
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+
+        return new JdbcTemplate(new HikariDataSource(config));
+    }
 
 
-	@Bean(name="dataSource")
-	public DataSource getDataSource() {
+    @Bean(name = "sqlSessionFactory")
+    public SqlSessionFactory getSqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(getDataSource());
 
-		HikariConfig config = new HikariConfig();
-		config.setDriverClassName(driver);
-		config.setJdbcUrl(url);
-		config.setUsername(username);
-		config.setPassword(password);
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        sqlSessionFactoryBean.setMapperLocations(resolver.getResources(mapperLocations));
 
-		return new HikariDataSource(config);
-	}
-
-	@Bean(name="jdbcTemplate")
-	public JdbcTemplate getJdbcTemplate() {
-		
-		HikariConfig config = new HikariConfig();
-		config.setDriverClassName(driver);
-		config.setJdbcUrl(url);
-		config.setUsername(username);
-		config.setPassword(password);
-		
- 		return new JdbcTemplate(new HikariDataSource(config));
-	}
-
-
-
-	@Bean(name="sqlSessionFactory")
-	public SqlSessionFactory getSqlSessionFactory() throws Exception {
-		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-		sqlSessionFactoryBean.setDataSource(getDataSource());
-
-		PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-		sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath:/mapper/*.xml"));
-
-		return sqlSessionFactoryBean.getObject();
-	}
+        return sqlSessionFactoryBean.getObject();
+    }
 
 }
